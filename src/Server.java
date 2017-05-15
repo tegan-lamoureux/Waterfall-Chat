be653814a6e77e8d8fb92e94dee559ff9ec12801
Tegan Lamoureux <contact@teux.me>
2017-05-14 19:59:59 -0700

    Started work on user list (and user functionality) in server.

--- src/Server.java
+++ src/Server.java
@@ -5,7 +5,68 @@ import java.util.Vector;
  * Created by tegan on 5/11/2017.
  */
 public class Server {
+    private class User{
+        private String username = null;
+        private String password = null;
+        private boolean logged_in = false;
+        private int client_id = 0;
+
+        public User(String username, String password){
+            this.username = username;
+            this.password = password;
+        }
+
+        public boolean set_username(String username){
+            if(username != null){
+                this.username = username;
+                return true;
+            }
+            else
+                return false;
+        }
+        public boolean set_password(String password){
+            if(password != null) {
+                this.password = password;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public boolean check_password(String password){
+            return (password == this.password);
+        }
+
+        public String get_username(){
+            return username;
+        }
+    }
+
     private Vector<Integer> connected_clients = new Vector<>();
+    private Vector<User> user_list = new Vector<>();
+
+    public boolean check_user_exists(String username){
+        boolean user_found = false;
+
+        for(User single: user_list)
+            if(single.get_username().equals(username))
+                user_found = true;
+
+        return user_found;
+    }
+
+    public boolean create_user_account(String username, String password){
+        if(username != null && password != null){
+            user_list.add(new User(username, password));
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public boolean login(){
+        return false;
+    }
 
     // Will "connect" a client to the server. What this entails is finding a unique 6-digit client ID and
     // adding that client ID to a list of "connected clients". It also returns the client ID, which the client
@@ -14,19 +75,18 @@ public class Server {
         Random random = new Random();
 
         int client_id = random.nextInt(999999) + 100000;
+        boolean valid_id = false;
 
         // Loop through all of our connected clients and check to see if the new client ID is unique.
         // If so, great, use it. If not, generate a new one and restart the search.
-        // Also watch out for an error condition, and if no result in 100000 tries, exit.
-        for(int i = 0, timeout = 100000; i < connected_clients.size() && timeout > 0; ++i, --timeout){
-            if(connected_clients.get(i) == client_id){ // if we found a duplicate ID
-                i = 0; // restart search
-                client_id = random.nextInt(999999) + 100000; // generate new ID
-                //System.out.println("ERROR: clid match; restarting.");
-            }
+        while(!valid_id){
+            valid_id = true; // assume validity until proven otherwise
+            for (int i: connected_clients) {
+                if (i == client_id) { // if we found a duplicate ID
+                    client_id = random.nextInt(999999) + 100000; // generate new ID
 
-            if(timeout == 0)
-                client_id = -1;
+                }
+            }
         }
 
         if(client_id != -1)
